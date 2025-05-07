e02358632b98a57578ae609a87ec554666f98c3c
Tude Prayatna <tudeprayatna04@gmail.com>
2025-05-08 07:44:11 +0800

    feat(add): integrate chat response  with backend

--- app/actions/post-chat.ts
+++ app/actions/post-chat.ts
@@ -1,5 +1,25 @@
 "use server";
 
-export const handlePostChat = async (input: string) => {
-  console.log(input);
+export const postChat = async (query: string) => {
+  const response = await fetch(`${process.env.API_URL}/ask`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
+  });
+
+  console.log(response);
+
+  if (!response.ok) {
+    return { success: false, message: "Failed to generate reponses" };
+  }
+
+  const message = await response.json();
+
+  return {
+    success: true,
+    message: "Success",
+    data: {
+      message,
+    },
+  };
 };
--- app/components/organisms/chat-body.tsx
+++ app/components/organisms/chat-body.tsx
@@ -1,76 +1,64 @@
+"use client";
+
 import React from "react";
 import ChatBubble from "../atoms/chat-bubble";
 import ChatInput from "../molecules/message-input";
-import { handlePostChat } from "@/app/actions/post-chat";
+import { postChat } from "@/app/actions/post-chat";
 
-const chats = [
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-  {
-    isUser: true,
-    message: "Hey, how are you?",
-  },
-  {
-    isUser: false,
-    message: "I am a robot that will help you through everything",
-  },
-];
+type MessageProps = {
+  role: "user" | "assistant";
+  content: string;
+};
 
 const ChatBody = () => {
+  const [chat, setChat] = React.useState<MessageProps[]>([]);
+  const [isPending, startTransition] = React.useTransition();
+
+  const handleMessage = (message: MessageProps) => {
+    setChat((prev) => [...prev, message]);
+  };
+
+  const handleSubmit = (input: string) => {
+    const userMessage: MessageProps = { role: "user", content: input };
+    handleMessage(userMessage);
+
+    startTransition(async () => {
+      const response = await postChat(input);
+
+      if (response.success && response.data) {
+        console.log(response.data.message);
+        handleMessage({
+          role: "assistant",
+          content: response.data.message.content!,
+        });
+      } else {
+        handleMessage({
+          role: "assistant",
+          content: "Maaf, terjadi kesalahan.",
+        });
+      }
+    });
+  };
+
   return (
     <div className="w-full h-screen flex justify-center bg-gray-950">
       <div className="w-1/2 flex flex-col items-stretch justify-between">
         <div className="flex flex-col overflow-y-auto px-2 py-10 gap-8">
           <div className="w-full mx-auto space-y-6">
-            {chats.map((chat, index) => (
+            {chat.map((chat, index) => (
               <ChatBubble
-                isUser={chat.isUser}
-                message={chat.message}
+                isUser={chat.role == "user"}
+                message={chat.content}
                 key={index}
               />
             ))}
+            {isPending && (
+              <ChatBubble message="Sedang membuat jawaban..." isUser={false} />
+            )}
           </div>
         </div>
         <div className="pb-4">
-          <ChatInput submitHandler={handlePostChat} />
+          <ChatInput submitHandler={handleSubmit} />
         </div>
       </div>
     </div>
